9566934278c7db1ee74fbc49a059e605636cc6af
lakovin <vlagdon01@gmail.com>
2025-04-11 21:13:03 +0600

    Initial commit

---
+++ mangaData.js
@@ -0,0 +1,99 @@
+// Список всех доступных манг
+const mangaList = [
+    {
+        code: "onepiece",
+        title: "One Piece",
+        image: "images/onepiece.jpg",
+        genres: ["Сёнен", "Приключения"]
+    },
+    {
+        code: "naruto",
+        title: "Naruto",
+        image: "images/naruto.jpg",
+        genres: ["Сёнен", "Ниндзя"]
+    },
+    {
+        code: "berserk",
+        title: "Berserk",
+        image: "images/Berserk.jpg",
+        genres: ["Сэйнен", "Дарк Фэнтези"]
+    },
+    {
+        code: "vagabond",
+        title: "Vagabond",
+        image: "images/Vagabond.jpg",
+        genres: ["Сэйнен", "Самурай"]
+    },
+    {
+        code: "sagavinland",
+        title: "Vinland Saga",
+        image: "images/sagavinland.jpg",
+        genres: ["Сэйнен", "Приключения"]
+    },
+    {
+        code: "ajin",
+        title: "Ajin",
+        image: "images/ajin.jpg",
+        genres: ["Сэйнен", "Мистика"]
+    },
+    {
+        code: "Punpun",
+        title: "Goodnight, Punpun",
+        image: "images/Punpun.jpg",
+        genres: ["Сэйнен", "Драма"]
+    },
+    {
+        code: "homunculus",
+        title: "Homunculus",
+        image: "images/Homunculus.jpg",
+        genres: ["Сэйнен", "Мистика"]
+    },
+    {
+        code: "Aot",
+        title: "Attack on Titan",
+        image: "images/aot.jpg",
+        genres: ["Сёнен", "Приключения"]
+    },
+    {
+        code: "bleach",
+        title: "Bleach",
+        image: "images/Bleach.jpg",
+        genres: ["Сёнен", "Фентези"]
+    },
+    {
+        code: "Deathnote",
+        title: "Death Note",
+        image: "images/Deathnote.jpg",
+        genres: ["Сэйнен", "Психология"]
+    },
+    {
+        code: "Demonslayer",
+        title: "Demon Slayer",
+        image: "images/demonslayer.jpg",
+        genres: ["Сёнен", "Демоны"]
+    },
+    {
+        code: "Evangelion",
+        title: "Evangelion",
+        image: "images/Evangelion.jpg",
+        genres: ["Сэйнен", "Психология"]
+    },
+    {
+        code: "Firepunch",
+        title: "Fire Punch",
+        image: "images/firepunch.jpg",
+        genres: ["Сэйнен", "Приключения"]
+    },
+    {
+        code: "Monster",
+        title: "Monster",
+        image: "images/Monster.jpg",
+        genres: ["Сэйнен", "Психология"]
+    },
+    {
+        code: "Jujutsukaisen",
+        title: "jujutsu Kaisen",
+        image: "images/Jujutsukaisen.jpg",
+        genres: ["Сёнен", "Мистика"]
+    },
+];
